cc64ff53960bb280f6940bd6d716bcb7dd0f6a8a
android_logicgrabber <android@logicgrabber.com>
2019-02-03 12:15:51 +0530

    BannerAdUtility Update

--- library/src/main/java/com/example/library/ads/BannerAdUtility.kt
+++ library/src/main/java/com/example/library/ads/BannerAdUtility.kt
@@ -1,5 +1,6 @@
 package com.example.library.ads;
 
+import com.example.common.util.MyLog
 import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
@@ -30,6 +31,35 @@ public class BannerAdUtility {
 
             override fun onAdFailedToLoad(p0: Int) {
                 super.onAdFailedToLoad(p0)
+                when (p0) {
+                    AdRequest.ERROR_CODE_INTERNAL_ERROR -> {
+                        MyLog().printLog(
+                            "AdRequest",
+                            "onAdFailedToLoad: code: ($p0 - ERROR_CODE_INTERNAL_ERROR) msg: Something happened internally; for instance, an invalid response was received from the ad server."
+                        )
+
+                    }
+                    AdRequest.ERROR_CODE_INVALID_REQUEST -> {
+                        MyLog().printLog(
+                            "AdRequest",
+                            "onAdFailedToLoad: code: ($p0 - ERROR_CODE_INTERNAL_ERROR) msg: The ad request was invalid; for instance, the ad unit ID was incorrect."
+                        )
+
+                    }
+                    AdRequest.ERROR_CODE_NETWORK_ERROR -> {
+                        MyLog().printLog(
+                            "AdRequest",
+                            "onAdFailedToLoad: code: ($p0 - ERROR_CODE_INTERNAL_ERROR) msg: The ad request was unsuccessful due to network connectivity."
+                        )
+
+                    }
+                    AdRequest.ERROR_CODE_NO_FILL -> {
+                        MyLog().printLog(
+                            "AdRequest",
+                            "onAdFailedToLoad: code: ($p0 - ERROR_CODE_INTERNAL_ERROR) msg: The ad request was successful, but no ad was returned due to lack of ad inventory."
+                        )
+                    }
+                }
             }
 
             override fun onAdClosed() {
